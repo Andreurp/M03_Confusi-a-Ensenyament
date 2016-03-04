b828ec807c727b500fec8d93aa66d864f486e378
Andreu <Andreu@Andreu1.cendrassos2.net>
2016-03-04 15:42:51 +0100

    commit initial

---
+++ ConfusioEnsenyament/src/main/java/net/andreu/confusio/Processar.java
@@ -0,0 +1,30 @@
+package net.andreu.confusio;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Processar extends DefaultHandler {
+	
+	public void startElement(String uri, String localName, String qName, Attributes attributes){
+		
+
+		
+	}
+	public void characters(char[] ch, int start, int length) {
+		
+		
+		
+	}
+	
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+
+		
+		
+	}
+	
+	public void endDocument() {
+
+
+	}
+}
